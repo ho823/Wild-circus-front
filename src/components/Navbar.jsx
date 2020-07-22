62476a30c95139cb084404b67b4b39fd3a197c9e
ho823 <amelyne.ho@hotmail.fr>
2020-07-22 11:49:49 +0200

    Navbar architecture done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Accueil</li>
+        <li>Spectacle et réservation</li>
+        <li>Près de chez vous</li>
+        <li>À propos</li>
+        <li>Contact</li>
+      </ul>
+      
+    </div>
+  );
+}
+
