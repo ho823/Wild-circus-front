42e6c995171c2d8165c5d3ef76fcef2ef24e5753
ho823 <amelyne.ho@hotmail.fr>
2020-07-22 19:53:17 +0200

    Architecture Maps done

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
 import styled from 'styled-components';
-import { Title } from '../Mainstyle';
+import { Title, Button } from '../Mainstyle';
 import laying from '../image/laying.png';
 
 const MapsTitle = styled(Title)`
+  width: 20%;
 
 `;
 
@@ -17,16 +18,32 @@ const Banner = styled.div`
   background-color: #FDFF9B;
 `;
 
+const SearchButton = styled(Button)`
+width: 20%;
+`;
+
+const SearchInput = styled.input`
+  padding: 0.6rem 0.8rem 0.6rem 0.8rem;
+  margin-right: 1rem;
+  width: 20%;
+`;
+
+const MapsContent = styled.div`
+
+
+`;
 
 export default function Maps() {
   return (
-    <div>
+    <>
       <Navbar />
+      <MapsContent>
       <MapsTitle>Les spectacles près de chez moi</MapsTitle>
-      <input></input>
-      <button>Chercher</button>
+      <SearchInput type="search" placeholder="Tapez votre code postal"></SearchInput>
+      <SearchButton type="submit">Chercher</SearchButton>
       <Banner>
       </Banner>
-    </div>
+    </MapsContent>
+    </>
   );
 }
